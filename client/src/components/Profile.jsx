7344bbc3bdae97d2532e382d109aada9ce85fcbf
LynaSim <lyna.cloud@gmail.com>
2026-05-08 00:27:37 +0100

    Implemented delete function

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,4 +1,7 @@
+// from delete-feature branch
+
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import api from '../api';
 import '../style.css';
 
@@ -19,30 +22,46 @@ function Profile() {
     fetchMyMemes();
   }, []);
 
+  const handleDelete = async (id) => {
+  try {
+    await api.delete(`/api/memes/${id}`);
+    
+    setUserMemes((prevMemes) => 
+      prevMemes.filter((meme) => String(meme.id) !== String(id))
+    );
+    
+    console.log("Meme deleted successfully");
+  } catch (error) {
+    console.error(
+      "Failed to delete meme:", 
+      error.response?.data?.message || error.message
+    );
+    alert("Could not delete meme. Are you logged in?");
+  }
+};
+
   return (
     <section className="container my-5 px-5 text-white">
       <h1>{username}'s Saved Memes</h1>
       <hr />
 
-    <div className="row g-5">
-    {userMemes.map(meme => (
+      <div className="row g-5">
+        {userMemes.map(meme => (
 
           <div key={meme.id} className="card col-12 col-md-6 col-lg-4 bg-dark">
-            <img src={meme.url} className="card-img-top img-fluid" alt="Meme"/>
-              <div className="card-body">
-                <p className="card-text text-info"> by: <span className="badge text-bg-warning">{meme.user.username || 'Anonymous'}</span><br/>on <span className="date">{new Date(meme.createdOn).toLocaleDateString()}</span></p>
-              </div>
-          </div>
-      ))}
-    </div>
-      {/* <div>
-        {userMemes.map((meme) => (
-          // Need the id for implementing deletion later
-          <div key={meme.id || meme._id}> 
-            <img src={meme.url} alt="meme" />
+            {/* <div className='text-white'>Meme ID number: {meme.id}</div> */}
+
+            <img src={meme.url} className="card-img-top img-fluid" alt="Meme" />
+            <div className="card-body">
+              <p className="card-text text-info"> by: <span className="badge text-bg-warning">{meme.user.username || 'Anonymous'}</span><br />on <span className="date">{new Date(meme.createdOn).toLocaleDateString()}</span></p>
+            </div>
+            <button
+              className="btn btn-danger mt-3 btn-sm"
+              onClick={() => handleDelete(meme.id)}>Delete Meme
+            </button>
           </div>
         ))}
-      </div> */}
+      </div>
     </section>
   );
 }
